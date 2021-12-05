261bbfd2f95ebc54d75afb800439f9230436e52d
tossarporn <tossarporn@gmail.com>
2021-12-06 01:23:50 +0700

    scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,13 @@ import 'package:flutter/material.dart';
 
 void main() {
   var app = MaterialApp(
-    title: "my App", 
-    home: Text("hellow dart")
-    );//MaterialApp
+    title: "my App",
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("hello flutter"),
+      ),
+      body: Text("hello people"),
+    ),
+  );
   runApp(app);
 }
