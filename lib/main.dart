630dcb35f6e4327e978187473bf23ac84cbd5a61
tossarporn <tossarporn@gmail.com>
2021-12-16 17:36:23 +0700

    api_connect

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 import 'FoodMenu.dart';
 import 'Money_Box.dart';
 import 'asychronous_future.dart';
+import 'package:http/http.dart' as htpp;
+
 void main() async {
   //var app = Myapp();
   //runApp(app); เขียนแบบสร้างตัวแปร
   runApp(Myapp());
-  print(await loginUser());//รอข้อมูล
-  
+  print(await loginUser()); //รอข้อมูล
 }
 
 //สร้าง widget แบบ StatelessWidget เปลี่ยนแปลงค่าไม่ได้ มีหน้าที่แสดง themeApp()
@@ -36,33 +37,35 @@ class _MyhomePageState extends State<MyhomePage> {
   @override
   void initState() {
     super.initState();
-    print("เรียกใช้งาน initState");
+    getExchangeRate();
   }
+
+  Future<void> getExchangeRate() async {
+    final url = Uri.parse("https://fakestoreapi.com/products/1");
+    //var response = await htpp.get(url);
+    htpp.Response response = await htpp.get(url);
+    if (response.statusCode == 200) {
+      print(response.body);
+    } else {
+      print("fails");
+    }
+  }
+
   //แสดงผลข้อมูล
   @override
   Widget build(BuildContext context) {
     print("เรียกใช้งาน build");
     return Scaffold(
-        appBar: AppBar(
-          title: Text("บัญชีของฉัน",
-              style: TextStyle(
-                  fontSize: 25,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold)),
-        ),
-        body: Column(
-          children: [
-            Text(number.toString(),style: TextStyle(fontSize: 30),)
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: (){
-            setState(() {
-              number++;
-            });
-          },
-          child: Icon(Icons.add),
-          ),  
+      appBar: AppBar(
+        title: Text("อัตราการแลกเปลี่ยนสกุลเงิน",
+            style: TextStyle(
+                fontSize: 25,
+                color: Colors.white,
+                fontWeight: FontWeight.bold)),
+      ),
+      body: Column(
+        children: [],
+      ),
     );
   }
 }
